745bd6a55834062aa0137cc420dfaf9780bbd5a3
bebekUX <briantana.869@gmail.com>
2022-11-30 20:49:13 +0700

    gd13

---
+++ app/src/main/java/com/example/gd11_b_0798/models/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.example.gd11_b_0798.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
